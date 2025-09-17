035ace1b9522636dfda9fa6673bd6687740c3f0c
Marvin at Home <marvinhuang@qq.com>
2025-09-17 17:32:15 +0800

    adapt lottery crawler python env for surface

--- 40_Python/Crawler/LotteryCrawler/national/main.bat
+++ 40_Python/Crawler/LotteryCrawler/national/main.bat
@@ -1,5 +1,11 @@
 @echo off
 
-"D:\Program Files\Python36\python.exe" main.py
+@REM GIGABYTE
+if exist "D:\Program Files\Python36" set python3="D:\Program Files\Python36\python.exe"
+
+@REM SURFACE
+if exist "D:\Program Files\Python39" set python3="D:\Program Files\Python36\python.exe"
+
+%python3% main.py
 
 pause
